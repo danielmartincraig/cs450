4fa2ed74a4cf764e5440e4f686f8fff272346e16
danielmartincraig <danielmartincraig@gmail.com>
2018-10-08 09:30:10 -0600

    Started prove04

---
+++ src/prove04.py
@@ -0,0 +1,16 @@
+###############################################################################
+#                              ID3 Decision Tree
+#                                Daniel Craig
+#
+#                            Milestone 1 - 10/8/18
+#
+###############################################################################
+
+class decisionTree(object):
+    def __init__(self):
+        pass
+
+# 1. Implement the basic ID3 decision tree algorithm
+# 2. Produce a textual view of your resulting tree
+# 3. Basic experimentation
+# 4. Compare to existing implementations
