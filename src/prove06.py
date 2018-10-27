502e09286e60623d24688165a8bdb1c48e179627
Daniel Craig <danielmartincraig@gmail.com>
2018-10-26 18:58:58 -0600

    Added prove06.py

---
+++ src/prove06.py
@@ -0,0 +1,21 @@
+###############################################################################
+#                           Neural Net Milestone 1
+#
+#                                Daniel Craig
+###############################################################################
+
+#  Create some type of mechanism to represent your nodes (i.e. neurons) and their
+#  associated weights.
+
+#  Provide a way to create a single layer of nodes of any number (this should be
+#  easily specified via a parameter).
+
+#  Account for a bias input.
+
+#  Be able to take input from a dataset instance (with an arbitrary number of
+#  attributes) and have each node produce an output (i.e., 0 or 1) according to
+#  its weights.
+
+#  Be able to load and process at least one dataset of your choice.
+
+#  You should appropriately normalize the data set.
