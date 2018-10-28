dade7407d2aba6f79a385c0f6c0539a6ea5f76fd
Daniel Craig <danielmartincraig@gmail.com>
2018-10-27 21:08:36 -0600

    Started neural net in earnest

--- src/prove06.py
+++ src/prove06.py
@@ -4,8 +4,13 @@
 #                                Daniel Craig
 ###############################################################################
 
+import numpy as np
+import random
+
 #  Create some type of mechanism to represent your nodes (i.e. neurons) and their
 #  associated weights.
+neuralNet = np.array
+inputVector = np.array
 
 #  Provide a way to create a single layer of nodes of any number (this should be
 #  easily specified via a parameter).
@@ -19,3 +24,19 @@
 #  Be able to load and process at least one dataset of your choice.
 
 #  You should appropriately normalize the data set.
+
+def addLayer(net, numberOfNodes):
+    randomFloat = lambda: random.uniform(-1,1)
+    if myNet == None:
+        pass
+    else:
+        pass
+
+###############################################################################
+# My neural net in practice
+###############################################################################  
+
+myNet = None
+myInputVector = inputVector([.5,.5])
+
+addLayer(myNet, numberOfNodes=5)
